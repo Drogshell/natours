88b09c0fca6f65f56d8b8d04935653504be8fabc
Drogshell <89346155+Drogshell@users.noreply.github.com>
2025-06-02 22:31:24 +1000

    Major Refactor - Added multiple files to separate concerns; controllers and routes - Minimised the code in the app.js file - Created a server.js file to be the entry point

--- app.js
+++ app.js
@@ -1,91 +1,14 @@
 const express = require('express');
-const fs = require('fs');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
+app.use(morgan('dev'));
 app.use(express.json());
 
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-app.get('/api/v1/tours/:id', (req, res) => {
-    const id = req.params.id * 1;
-
-    if (id > tours.length) {
-        return res.status(404).json({
-            status: 'failed',
-            message: `${id} doesn't exist`,
-        });
-    }
-
-    const tour = tours.find((el) => el.id === id);
-
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tour,
-        },
-    });
-});
-
-app.post('/api/v1/tours', (req, res) => {
-    const newID = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign(newID, req.body);
-
-    tours.push(newTour);
-    fs.writeFile(
-        `${__dirname}/dev-data/data/tours-simple.json`,
-        JSON.stringify(tours),
-        (err) => {
-            res.status(201).json({
-                status: 'Success',
-                data: {
-                    tour: newTour,
-                },
-            });
-        }
-    );
-    res.send('DONE!!');
-});
-
-app.patch('/api/v1/tours/:id', (req, res) => {
-    //This is how you would update just the specific information that needs to be updated.
-    const id = req.params.id * 1;
-
-    if (id > tours.length) {
-        return res.status(404).json({
-            status: 'failed',
-            message: `${id} doesn't exist`,
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: 'Updated Tour',
-        },
-    });
-});
-
-app.delete('/api/v1/tours/:id', (req, res) => {
-    //This is how you would update just the specific information that needs to be updated.
-    const id = req.params.id * 1;
-
-    if (id > tours.length) {
-        return res.status(404).json({
-            status: 'failed',
-            message: `${id} doesn't exist`,
-        });
-    }
-
-    res.status(204).json({
-        status: 'success',
-        data: null,
-    });
-});
-
-const port = 3000;
-app.listen(port, () => {
-    console.log(`App is listening on port ${port}`);
-});
+module.exports = app;
---
+++ controllers/tourController.js
@@ -0,0 +1,91 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.getAllTours = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tours,
+        },
+    });
+};
+
+exports.getTour = (req, res) => {
+    const id = req.params.id * 1;
+
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'failed',
+            message: `${id} doesn't exist`,
+        });
+    }
+
+    const tour = tours.find((el) => el.id === id);
+
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tour,
+        },
+    });
+};
+
+exports.createTour = (req, res) => {
+    const newID = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign(newID, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        (err) => {
+            res.status(201).json({
+                status: 'Success',
+                data: {
+                    tour: newTour,
+                },
+            });
+        }
+    );
+    res.send('DONE!!');
+};
+
+exports.updateTour = (req, res) => {
+    //This is how you would update just the specific information that needs to be updated.
+    const id = req.params.id * 1;
+
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'failed',
+            message: `${id} doesn't exist`,
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: 'Updated Tour',
+        },
+    });
+};
+
+exports.deleteTour = (req, res) => {
+    //This is how you would update just the specific information that needs to be updated.
+    const id = req.params.id * 1;
+
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'failed',
+            message: `${id} doesn't exist`,
+        });
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null,
+    });
+};
